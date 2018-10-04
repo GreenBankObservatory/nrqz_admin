eb3b068c1c4dc60ac19f7c76bcf11528b3ae883f
Thomas Chamberlin <tchamber@nrao.edu>
2018-10-04 10:58:38 -0400

    Updated menu so that all things are accessible from homepage
    Also now redirect / to /submission so that you can't get lost as easily

--- nrqz_admin/urls.py
+++ nrqz_admin/urls.py
@@ -2,8 +2,9 @@
 
 from django.contrib import admin
 from django.urls import include, path
-
+from  django.views.generic.base import RedirectView
 urlpatterns = [
+    path('', RedirectView.as_view(url="/submission")),
     path('submission/', include('submission.urls')),
     path('explorer/', include('explorer.urls')),
     path('admin/', admin.site.urls),
