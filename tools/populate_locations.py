207616e560be16e3c15f8e8160c5c4b194efa0ff
Thomas Chamberlin <tchamber@nrao.edu>
2018-11-06 19:15:52 -0500

    Created populate_locations; populates Facility.location

---
+++ tools/populate_locations.py
@@ -0,0 +1,12 @@
+from django.contrib.gis.geos.error import GEOSException
+
+from cases.models import Facility
+
+def run():
+    for facility in Facility.objects.filter(longitude__isnull=False, latitude__isnull=False):
+        point_str = f"Point({facility.longitude} {facility.latitude})"
+        try:
+            facility.location = GEOSGeometry(point_str)
+            facility.save()
+        except GEOSException as error:
+            print(f"Error saving {point_str}: {error}")
