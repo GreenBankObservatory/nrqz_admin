edfdd78576a1bdde37c429654b95cda993be24b6
Thomas Chamberlin <tchamber@nrao.edu>
2019-03-26 15:50:39 -0400

    Updated prod settings

--- nrqz_admin/settings/production.py
+++ nrqz_admin/settings/production.py
@@ -1,2 +1,23 @@
-ALLOWED_HOSTS = []
+from .base import *
+
+ALLOWED_HOSTS = ["galileo", "galileo.gb.nrao.edu"]
+
+INSTALLED_APPS += ["django_extensions"]
+
 DEBUG = False
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": "nrqz_admin_prod",
+        "USER": "nrqz",
+        "PASSWORD": "potato",
+        "HOST": "galileo.gb.nrao.edu",
+        "PORT": "5432",
+    },
+}
+
+SECRET_KEY = "nw20&ip%@lhstow7-u6!dx_+f@a#&93z3784$0_@_m-#@@dher"
+
+NRQZ_LETTER_TEMPLATE_DIR = "/home/sandboxes/tchamber/repos/nrqz_admin/letter_templates"
+NRQZ_ATTACHMENT_DIR = "/home/sandboxes/tchamber/projects/nrqz_admin"
