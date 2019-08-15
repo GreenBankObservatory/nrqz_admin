e68f12fba0f34fbf37c392e142e254132aea77fd
Thomas Chamberlin <tchamber@nrao.edu>
2019-08-15 09:23:43 -0400

    Added STATIC_ROOT to production settings

--- nrqz_admin/settings/production.py
+++ nrqz_admin/settings/production.py
@@ -24,3 +24,4 @@ SECRET_KEY = "nw20&ip%@lhstow7-u6!dx_+f@a#&93z3784$0_@_m-#@@dher"
 
 NRQZ_LETTER_TEMPLATE_DIR = "/home/sandboxes/tchamber/repos/nrqz_admin/letter_templates"
 NRQZ_ATTACHMENT_DIR = "/home/sandboxes/tchamber/projects/nrqz_admin"
+STATIC_ROOT = "/home/nrqz.gb.nrao.edu/content"
