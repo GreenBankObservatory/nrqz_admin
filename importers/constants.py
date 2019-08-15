f01b06ec7279462855a8df72bb372c59e4144c1f
Thomas Chamberlin <tchamber@nrao.edu>
2019-08-15 16:09:22 -0400

    Raised max valid case number

--- importers/constants.py
+++ importers/constants.py
@@ -2,5 +2,5 @@
 
 # Empirically determined. This is solely to avoid obviously-wrong
 # case numbers from being imported
-MAX_VALID_CASE_NUMBER = 12000
+MAX_VALID_CASE_NUMBER = 99000
 MIN_VALID_CASE_NUMBER = 100
