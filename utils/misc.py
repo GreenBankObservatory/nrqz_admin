ec505fc4b8d5cb3a16b0874c2ac87b5d0acc3b22
Thomas Chamberlin <tchamber@nrao.edu>
2019-09-13 12:56:26 -0400

    Fixed attachment links for web resources

--- utils/misc.py
+++ utils/misc.py
@@ -1,4 +1,13 @@
 def to_file_link(path):
+    # If path starts with a \, assume that it is a network path
+    # (e.g. \\gbfiler). These paths must be prefixed with extra backslashes in
+    # order to function properly
     if path.startswith(r"\\"):
         return f"file://///{path[2:]}"
+
+    # If path starts with http, treat it as a web link and make no changes
+    # to it
+    if path.lower().startswith("http"):
+        return path
+
     return f"file://{path}"
