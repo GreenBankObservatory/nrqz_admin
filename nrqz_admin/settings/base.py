7e26f643f5f6c9a55552b517bb2e47e313eff4b6
Thomas Chamberlin <tchamber@nrao.edu>
2021-03-31 12:44:37 -0400

    Enable Sentry

--- nrqz_admin/settings/base.py
+++ nrqz_admin/settings/base.py
@@ -3,6 +3,13 @@
 import os
 from pathlib import Path
 
+import environ
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+env = environ.Env()
+environ.Env.read_env()
+
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
@@ -209,3 +216,15 @@ PATH_REMAPPINGS_BY_CLIENT_HOST = {
 #     }
 # }
 # USER_AGENTS_CACHE = "default"
+
+sentry_sdk.init(
+    dsn=env("SENTRY_DSN"),
+    integrations=[DjangoIntegration()],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+)
